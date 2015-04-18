f91b9674262476065e7f7ecfcfeb4e480eb39d34
chenmuen <593827475@qq.com>
2015-04-18 15:08:20 +0800

    布局更新

--- ThreebodyShip/src/cross/threebodyship/model/Mode.java
+++ ThreebodyShip/src/cross/threebodyship/model/Mode.java
@@ -4,23 +4,31 @@ import java.util.ArrayList;
 
 public class Mode {
 	public String modeName;
+	public String[] chap = new String[]{"chap00","chap01","chap02"};
 	public ArrayList<Stage> stages = new ArrayList<Stage>();
-	int modeType;
+	public int modeType;
 	
 	public Mode(String modeName){
 		this.modeName = modeName;
-//		switch (modeName) {
-//		case "故事模式":
-//			modeType = 1;
-//			break;
-//		default:
-//			break;
-//		}
-//			
-//		if(modeType == 1){
-//			stages.add(new Stage("一而生二", "Ch1-Stage1-Before.png", "Ch1-Stage1-After.png"));
-//			stages.add(new Stage("二而生三", "Ch1-Stage1-Before.png", "Ch1-Stage1-After.png"));
-//		}
+		switch (modeName) {
+		case "故事模式":
+			modeType = 1;
+			break;
+		default:
+			break;
+		}
+			
+		if(modeType == 1){
+			stages.add(new Stage("从一开始", "Ch1-Stage1-Before.png", "Ch1-Stage1-After.png"));
+			stages.add(new Stage("一而生二", "Ch1-Stage1-Before.png", "Ch1-Stage1-After.png"));
+			stages.add(new Stage("二而生三", "Ch1-Stage1-Before.png", "Ch1-Stage1-After.png"));
+			stages.add(new Stage("三相跌生", "Ch1-Stage1-Before.png", "Ch1-Stage1-After.png"));
+			stages.add(new Stage("黄金三角", "Ch1-Stage1-Before.png", "Ch1-Stage1-After.png"));
+			stages.add(new Stage("魔鬼三角", "Ch1-Stage1-Before.png", "Ch1-Stage1-After.png"));
+			stages.add(new Stage("魔鬼三角", "Ch1-Stage1-Before.png", "Ch1-Stage1-After.png"));
+			stages.add(new Stage("魔鬼三角", "Ch1-Stage1-Before.png", "Ch1-Stage1-After.png"));
+			stages.add(new Stage("魔鬼三角", "Ch1-Stage1-Before.png", "Ch1-Stage1-After.png"));
+		}
 	}
 	
 	
