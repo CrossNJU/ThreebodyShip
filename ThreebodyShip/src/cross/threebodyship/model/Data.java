eb77338fc6d8de334eb99d088731fde6078eef7c
RickChem <1395314348@qq.com>
2015-05-03 09:50:10 +0800

    Final

--- ThreebodyShip/src/cross/threebodyship/model/Data.java
+++ ThreebodyShip/src/cross/threebodyship/model/Data.java
@@ -1,63 +1,5 @@
-<<<<<<< HEAD
-package cross.threebodyship.model;
+//<<<<<<< HEAD
 
-import java.util.ArrayList;
-
-public class Data {
-	public ArrayList<Star> starList;
-	public Ship ship;
-	
-	public Data(int gameNumber){
-		starList = new ArrayList<Star>();
-		ship = new Ship();
-		
-		switch(gameNumber){
-		case 0:setGame0();break;
-		case 1:setGame1();break;
-		}
-	}
-	
-	public void setGame0(){
-		Star star = new Star();
-		
-		//初始化飞船
-		ship.setMass(1000);
-		ship.setSize(10);
-		ship.setSpeed(4);
-		ship.setState(true);
-		ship.outOfBorder = false;
-		
-		//初始化星球
-		star.setLocation(512, 518);
-		star.setSize(366);
-		star.setGravityScope(710);
-		
-		starList.add(star);
-	}
-	
-	public void setGame1(){
-		Star starOne = new Star();
-		Star starTwo = new Star();
-		//初始化飞船
-		ship.setMass(1000);
-		ship.setSize(10);
-		ship.setSpeed(4);
-		ship.setState(true);
-		ship.outOfBorder = false;
-		//初始化星球
-		
-		starOne.setLocation(400,200);
-		starOne.setSize(180);
-		starOne.setGravityScope(350);
-		
-		starTwo.setLocation(670,500);
-		starTwo.setSize(180);
-		starTwo.setGravityScope(350);
-		
-	}
-	
-}
-=======
 package cross.threebodyship.model;
 
 import java.util.ArrayList;
@@ -119,4 +61,4 @@ public class Data {
 	}
 	
 }
->>>>>>> origin/master
+//>>>>>>> origin/master
