21d7ef80cb2a132ddfde4467b6ee3a9335574a52
RickChem <1395314348@qq.com>
2015-05-06 22:44:15 +0800

    change

--- ThreebodyShip/src/cross/threebodyship/model/Game.java
+++ ThreebodyShip/src/cross/threebodyship/model/Game.java
@@ -87,7 +87,7 @@ public class Game extends Observable implements Runnable{
 				(ship.getLocation().y-starList.get(i).getLocation().y)
 				);
 
-			if(distance<starList.get(i).getSize()/2){
+			if(distance-ship.getSize()/2<starList.get(i).getSize()/2){
 				if(starList.get(i).style.equals("BlackHole")){
 					BlackHole blackHole = (BlackHole)starList.get(i);
 					
@@ -152,6 +152,19 @@ public class Game extends Observable implements Runnable{
 //			System.out.println(f.f+" "+f.theta);
 		}
 		
+		//计算是否撞到行星
+		for(int i =0;i< planets.size(); i++){
+			double distance = Math.sqrt((ship.getLocation().x-planets.get(i).location.x)*
+					(ship.getLocation().x-planets.get(i).location.x)+
+					(ship.getLocation().y-planets.get(i).location.y)*
+					(ship.getLocation().y-planets.get(i).location.y));
+			
+			if(distance-ship.getSize()/2<planets.get(i).size/2) {
+				ship.setState(false);
+				break;
+			}
+		}
+		
 		//如果进入引力区
 		if((isInScope)&&(!ship.isRound)){
 //			System.out.println(1);
@@ -172,7 +185,7 @@ public class Game extends Observable implements Runnable{
 			
 			double theta2 = Math.atan(vy/vx);
 //			System.out.println(1);
-			System.out.println("vx:"+vx);
+//			System.out.println("vx:"+vx);
 			//修改theta2
 			if(ship.getDegreeToEast()<=Math.PI/2){
 				if(theta2<0) theta2+=Math.PI*2;
@@ -233,7 +246,7 @@ public class Game extends Observable implements Runnable{
 		ship = data.ship;
 		starList = data.starList;
 		planets = data.planets;
-		System.out.println("planets:"+planets.size());
+//		System.out.println("planets:"+planets.size());
 		//启动planets线程
 		for(int i = 0; i<planets.size(); i++){
 			Thread t = new Thread(planets.get(i));
