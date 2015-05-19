5ddef8ed6a6a4473f1bbf7af969bec17f549bd79
chenmuen <593827475@qq.com>
2015-05-20 00:19:07 +0800

    challengeUI

--- ThreebodyShip/src/cross/threebodyship/userinterface/ChallengeUI.java
+++ ThreebodyShip/src/cross/threebodyship/userinterface/ChallengeUI.java
@@ -1,12 +1,70 @@
 package cross.threebodyship.userinterface;
 
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import cross.threebodyship.listener.EnterStageButtonListener;
+import cross.threebodyship.listener.ScrollListener;
+
 public class ChallengeUI extends JPanel {
-	
+	int panelWidth;
+	int panelHeight;
+
 	public ChallengeUI(SelectorUI selectorUI) {
-		setSize(MainUI.WIDTH, MainUI.HEIGHT);
+		panelWidth = (int) (MainUI.WIDTH * 0.7);
+		panelHeight = MainUI.HEIGHT;
+		setSize(panelWidth, panelHeight);
 		setOpaque(false);
 		setBackground(null);
+		
+		
+		// num表示的是关卡数，i表示的是章节数，j表示每个章节的第几关
+		int num = 0;
+		for (int i = 0; i < selectorUI.selector.mode.get(1).chap.length; i++) {
+			JLabel chapLabel = new JLabel();
+			chapLabel.setBounds((int) (panelWidth * 0.2) - 40,
+					(int) (panelHeight * 0.3) * (i + 1) - 100, 220, 60);
+			chapLabel.setIcon(new ImageIcon("img/Selector/chap0" + i + ".png"));
+			chapLabel.setForeground(Color.BLACK);
+			chapLabel.setVisible(true);
+			add(chapLabel);
+
+			for (int j = 0; j < 3; j++) {
+				if (num == selectorUI.selector.mode.get(1).stages.size()) {
+					break;
+				}
+				JButton stageButton = new JButton(
+						selectorUI.selector.mode.get(1).stages.get(num).title);
+
+				stageButton.setBounds((int) (panelWidth * 0.25) * (j + 1) - 80,
+						(int) (panelHeight * 0.3) * (i + 1) - 40, 154, 154);
+				stageButton.setVisible(true);
+				stageButton.addMouseListener(new EnterStageButtonListener(
+						selectorUI.mainPanel,
+						selectorUI.selector.mode.get(1).stages.get(num)));
+
+				String imageString = "img/Button/stagebtn/btn-challenge"
+						+ (num + 1) + "-normal.png";
+				String imageHoverString = "img/Button/stagebtn/btn-challenge"
+						+ (num + 1) + "-hover.png";
+				ImageIcon image = new ImageIcon(imageString);
+				ImageIcon imageHover = new ImageIcon(imageHoverString);
+
+				stageButton.setIcon(image);
+				stageButton.setRolloverIcon(imageHover);
+				stageButton.setContentAreaFilled(false);
+				stageButton.setBorderPainted(false);
+				stageButton.setFocusPainted(false);
+
+				add(stageButton);
+				num++;
+			}
+		}
+
+		addMouseWheelListener(new ScrollListener(this));
 	}
 }
