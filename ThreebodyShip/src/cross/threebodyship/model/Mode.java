9d069184e5234d64b8cefe4142a69ced4553e3c4
chenmuen <593827475@qq.com>
2015-05-19 20:18:25 +0800

    修改

--- ThreebodyShip/src/cross/threebodyship/model/Mode.java
+++ ThreebodyShip/src/cross/threebodyship/model/Mode.java
@@ -10,13 +10,13 @@ import java.util.Scanner;
 
 public class Mode {
 	public String modeName;
-	public String[] chap = new String[]{"chap00","chap01","chap02","chap03","chap04","chap05"};
+	public String[] chap;
 	public ArrayList<Stage> stages = new ArrayList<Stage>();
 	public int modeType;
-	
+
 	public int num;
-	
-	public Mode(String modeName){
+
+	public Mode(String modeName) {
 		this.modeName = modeName;
 		switch (modeName) {
 		case "故事模式":
@@ -31,12 +31,14 @@ public class Mode {
 		case "设置":
 			modeType = 4;
 			break;
-				
+
 		default:
 			break;
 		}
-			
-		if(modeType == 1){
+
+		if (modeType == 1) {
+			chap= new String[] { "chap00", "chap01", "chap02",
+						"chap03", "chap04", "chap05" };
 			addStage(new Stage("从一开始"));
 			addStage(new Stage("一而生二"));
 			addStage(new Stage("二而生三"));
@@ -55,43 +57,54 @@ public class Mode {
 			addStage(new Stage("Game16"));
 			addStage(new Stage("Game17"));
 			addStage(new Stage("Game18"));
-		
-		
-//		System.out.println("stages:"+stages.size());
-		//读出数据
-			num = 0;
-		BufferedReader reader = null;
-		try {
-			reader = new BufferedReader(new FileReader((new File("data.txt"))));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} else if (modeType == 2) {
+//			挑战模式章节
+			chap = new String[]{"challenge1"};
+//			加入挑战模式关卡
 		}
 		
-		try {
-			num = Integer.parseInt(reader.readLine());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
-//		System.out.println(num);
-		for (int i = 0; i < num; i++) {
-			stages.get(i).isLocked = false;
-		}
-		
+		// System.out.println("stages:"+stages.size());
+		// 读出数据
+//		num = 0;
+//		BufferedReader reader = null;
+//		try {
+//			reader = new BufferedReader(new FileReader((new File("data.txt"))));
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//		try {
+//			num = Integer.parseInt(reader.readLine());
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//		// System.out.println(num);
+//		for (int i = 0; i < num; i++) {
+//			stages.get(i).isLocked = false;
+//		}
+
+	}
+
+	public void addStage(Stage stage) {
+		if(modeType==1){
+			if (stages.size() > 0) {
+				stages.get(stages.size() - 1).nextStage = stage;
+			}
+			stages.add(stage);
+			stage.num = stages.size();
+			stage.game.gameNumber = stage.num;
+		}else if(modeType==2){
+			if (stages.size() > 0) {
+				stages.get(stages.size() - 1).nextStage = stage;
+			}
+			stages.add(stage);
+			stage.num = stages.size()+18;
+			stage.game.gameNumber = stage.num;
 		}
 	}
-	
-	public void addStage(Stage stage){
-		if(stages.size()>0){
-			stages.get(stages.size()-1).nextStage = stage;
-		}
-		stages.add(stage);
-		stage.num = stages.size();
-		stage.game.gameNumber = stage.num;
-//		stage.game.data = new Data(stage.game.gameNumber);
-	}
-	
-	
+
 }
